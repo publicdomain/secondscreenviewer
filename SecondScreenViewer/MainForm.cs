4469dfaa664856151d5ec23b2d9ab05e4f2a2582
Victor L. Senior <publicdomainvic@gmail.com>
2019-09-11 01:57:17 -0400

    Add initial project files

---
+++ SecondScreenViewer/MainForm.cs
@@ -0,0 +1,104 @@
+﻿// <copyright file = "MainForm.cs" company="PublicDomain.com">
+//     CC0 1.0 Universal (CC0 1.0) - Public Domain Dedication
+//     https://creativecommons.org/publicdomain/zero/1.0/legalcode
+// </copyright>
+
+namespace SecondScreenViewer
+{
+    // Directives
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Reflection;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Main form.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// The assembly version.
+        /// </summary>
+        private Version assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version;
+
+        /// <summary>
+        /// The semantic version.
+        /// </summary>
+        private string semanticVersion = string.Empty;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:SecondScreenViewer.MainForm"/> class.
+        /// </summary>
+        public MainForm()
+        {
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            this.InitializeComponent();
+
+            /* Semantic versioning */
+
+            // Set semantic version
+            this.semanticVersion = this.assemblyVersion.Major + "." + this.assemblyVersion.Minor + "." + this.assemblyVersion.Build;
+        }
+
+        /// <summary>
+        /// Handles the new tool strip menu item click event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnNewToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code.
+        }
+
+        /// <summary>
+        /// Handles the exit tool strip menu item click event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnExitToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code.
+        }
+
+        /// <summary>
+        /// Handles the headquarters at patreon.com tool strip menu item click event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnHeadquartersPatreoncomToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code.
+        }
+
+        /// <summary>
+        /// Handles the source code at github.com tool strip menu item click event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnSourceCodeGithubcomToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code.
+        }
+
+        /// <summary>
+        /// Handles the about tool strip menu item click event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnAboutToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code.
+        }
+
+        /// <summary>
+        /// Handles the view button click event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnViewButtonClick(object sender, EventArgs e)
+        {
+            // TODO Add code.
+        }
+    }
+}
---
+++ SecondScreenViewer/ScreenCaptureForm.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="ScreenCaptureForm.cs" company="PublicDomain.com">
+//     CC0 1.0 Universal (CC0 1.0) - Public Domain Dedication
+//     https://creativecommons.org/publicdomain/zero/1.0/legalcode
+// </copyright>
+namespace PublicDomain
+{
+    // Directives
+    using System;
+    using System.Drawing;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Description of ScreenCaptureForm.
+    /// </summary>
+    public partial class ScreenCaptureForm : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:PublicDomain.ScreenCaptureForm"/> class.
+        /// </summary>
+        public ScreenCaptureForm()
+        {
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles the screen capture form form closed event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnScreenCaptureFormFormClosed(object sender, FormClosedEventArgs e)
+        {
+            // TODO Add code.
+        }
+
+        /// <summary>
+        /// Handles the topmost tool strip menu item click event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnTopmostToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code.
+        }
+
+        /// <summary>
+        /// Handles the close tool strip menu item click event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnCloseToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code.
+        }
+    }
+}
